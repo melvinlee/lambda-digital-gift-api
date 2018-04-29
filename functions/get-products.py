79f26fe3db9cf14374dd7c3978126fa71f41a9a4
Lee Chin Yong <melvinlee@hotmail.com>
2018-04-29 13:08:40 +0800

    feat: added get-products function.

---
+++ functions/get-products.py
@@ -0,0 +1,26 @@
+import os
+import json
+import logging
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+# logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+table_name = os.getenv("PRODUCT_TABLE")
+
+
+def handler(event, context):
+    try:
+        table = dynamodb.Table(table_name)
+        response = table.scan()
+        return build_response(404)
+    except Exception as error:
+        return build_response(500, logger.error(error))
+
+
+def build_response(status_code, body=None):
+    if body:
+        return {'statusCode': status_code, 'body': json.dumps(body)}
+    return {'statusCode': status_code}
